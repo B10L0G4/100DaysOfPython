08754ecd5e1a1acdbd74bac6bed2b724bfe57847
B10l0g4 <vandyyh@gmail.com>
2023-09-01 05:17:01 -0300

    rollercoaster

---
+++ exercicios/Day3/rollercoster.py
@@ -0,0 +1,8 @@
+print(" Welcome to the Rollercoster! ")
+
+height = float(input('Whats is your height in cm? '))
+
+if height >= 120:
+    print('You able to ride in the rollercoaster! Buy your ticket')
+else:
+    print('You unable to ride! Sorry, you have to grow taller before you can ride. ')
