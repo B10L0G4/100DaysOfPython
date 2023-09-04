8a7186afdcd905ca5dc66c56021fed6acfe9d3b7
B10l0g4 <vandyyh@gmail.com>
2023-09-04 07:22:52 -0300

    refactoring

--- exercicios/Day3/rollercoster.py
+++ exercicios/Day3/rollercoster.py
@@ -4,24 +4,23 @@ height = float(input('Whats is your height in cm? '))
 
 if height >= 120:
     print('You able to ride in the rollercoaster! Buy your ticket')
-    photo = input('You want a photo?  \n')
-    photo = photo.upper()
-    if photo == 'YES':
-        print('Was add $3 to your ticket ')
-        age = int(input('What is your age?  '))
-        if age >= 18:
-            print('Your ticket is $21')
-        elif age < 12:
-            print('Your ticket is $10')
-        else:
-            print('Your ticket is $15')
+    bill = 0
+    age = int(input('What is your age?  '))
+    if age >= 18:
+        bill = 18
+        print(f'Your ticket is ${bill}')
+    elif age < 12:
+        bill = 7
+        print(f'Your ticket is ${bill}')
     else:
-        age = int(input('What is your age?  '))
-        if age >= 18:
-            print('Your ticket is $18')
-        elif age < 12:
-            print('Your ticket is $7')
-        else:
-            print('Your ticket is $12')
+        bill = 12
+        print(f'Your ticket is ${bill}')
+    photo = input('You want a photo? Y or N  \n')
+    photo = photo.upper()
+    if photo == 'Y':
+        bill = bill + 3
+        print(f'Your ticket is ${bill}')
+    else:
+        print(f'Your ticket is ${bill}')
 else:
     print('You unable to ride! Sorry, you have to grow taller before you can ride. ')
