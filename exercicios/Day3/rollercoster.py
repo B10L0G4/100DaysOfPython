c95b7a43b9884cef5d322ba00e3e8c2504955bed
B10l0g4 <vandyyh@gmail.com>
2023-09-04 07:07:38 -0300

    if aninhado

--- exercicios/Day3/rollercoster.py
+++ exercicios/Day3/rollercoster.py
@@ -4,12 +4,24 @@ height = float(input('Whats is your height in cm? '))
 
 if height >= 120:
     print('You able to ride in the rollercoaster! Buy your ticket')
-    age = int(input('What is your age?  '))
-    if age >= 18:
-        print('Your ticket is $18')
-    elif age < 12:
-        print('Your ticket is $7')
+    photo = input('You want a photo?  \n')
+    photo = photo.upper()
+    if photo == 'YES':
+        print('Was add $3 to your ticket ')
+        age = int(input('What is your age?  '))
+        if age >= 18:
+            print('Your ticket is $21')
+        elif age < 12:
+            print('Your ticket is $10')
+        else:
+            print('Your ticket is $15')
     else:
-        print('Your ticket is $12')
+        age = int(input('What is your age?  '))
+        if age >= 18:
+            print('Your ticket is $18')
+        elif age < 12:
+            print('Your ticket is $7')
+        else:
+            print('Your ticket is $12')
 else:
     print('You unable to ride! Sorry, you have to grow taller before you can ride. ')
