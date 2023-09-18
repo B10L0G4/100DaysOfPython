95aee908a27c1c9b7d363400f5ab526d0581ecbf
B10l0g4 <vandyyh@gmail.com>
2023-09-18 10:40:03 -0300

    gerador de password , versão linear, fácil

---
+++ exercicios/Day5/password_generator.py
@@ -0,0 +1,44 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+
+lt = ''
+for letter in range(0, len(letters)):
+    if nr_letters > letter:
+        letter = letters[letter]
+        lt = lt +letter
+nn = ''
+for num in range(0, len(numbers)):
+    if nr_numbers> num:
+        num = numbers[num]
+        nn = nn + num
+
+sy = ''
+for sym in range(0, len(symbols)):
+    if nr_symbols > sym:
+        sym = symbols[sym]
+        sy = sy + sym
+
+print(f'letras {lt} , numeros {nn}, simbolos {sy}')
+print(f'{lt}{nn}{sy}')
+
+
+
+
+
+
+
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
