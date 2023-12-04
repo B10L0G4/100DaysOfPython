b6ee61838584f806663eeaf59b79ff7bae6414b9
B10l0g4 <vandyyh@gmail.com>
2023-12-04 18:09:15 -0300

    desafio ano bissexto e meses

--- exercicios/Day10/mouth_years_count.py
+++ exercicios/Day10/mouth_years_count.py
@@ -4,8 +4,6 @@ def count_years(year):
     hundred = year % 100
     four_hundred = year % 400
 
-    print(four, hundred, four_hundred)
-
     if four == 0:
         while hundred >= 0:
             while four_hundred >= 0:
@@ -13,24 +11,60 @@ def count_years(year):
     else:
         return False
 
-def odd_even(month):
-    month = month % 2
+def odd_even(mounth):
+    mounth = mounth % 2
 
-if month == 0:
-    par = True
-    return par
-else:
-    impar = False
-    return impar
+    if mounth == 0:
+        par = True
+        return par
+    else:
+        impar = False
+        return impar
 
-def days_in_month(year, month):
+def days_in_month(year, mounth):
+    
     month_day = [31,30,29,28]
-    months= [Jan, Fev, Mar, Abril, Maio, Jun, Jul, Ago, Set, Out, Nov, Dez]
+    months=['Jan', 'Fev', 'Mar', 'Abril', 'Maio', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
 
+    a = ''
+    for mon in months:
+        mon = mon + a
+    mes=months[mounth -1]
+        
     for m in month_day:
-        print(m)
-        print(month_day)
+        continue
+        
+    par = month_day[1]
+    impar= month_day[0]
+    fev_par = month_day[2]
+    fev_impar = month_day[3]
+
+    if count_years(year) == True:
+        if odd_even(mounth) == True:
+            if mounth == 2:
+                print(f'O mês de {mes} tem {fev_par} dias e o ano de {year} é bissexto')
+            else:
+                print(f'O mês {mes} tem {par} dias e o ano de {year} é bissexto')
+        else:
+            print(f'O mês {mes} tem {impar} dias e o ano de {year} é bissexto')
+    else:
+        if odd_even(mounth) == True:
+            if mounth == 2:
+                print(f'O mês de {mes} tem {fev_impar} e o ano de {year} não é bissexto')
+            else:
+                print(f'O mês {mes} tem {par} dias e o ano de {year} não é bissexto')
+            
+        else:
+            print(f'O mês {mes} tem {impar} e o ano de {year} não é bissexto')
+
+       
+        
 
 year = int(input('Which year you want to check? '))
 mounth = int(input('Which mounth you want to check'))
-count_years(year)
+
+days_in_month(year, mounth)
+
+
+       
+
